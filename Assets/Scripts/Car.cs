9940b4bdfdbee52fb41700302ae3e1d1ef4e8a70
Miquel <miquelgalianallorca@gmail.com>
2017-12-14 22:00:10 +0100

    Basic force movement
    Basic force movement on car.

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour {
+
+	Rigidbody rigidBody;
+	public float accelerationForce = 100;
+	public float rotationForce = 100;
+
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> (); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Space))
+			rigidBody.AddForce (transform.forward * accelerationForce, ForceMode.Acceleration);
+
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical   = Input.GetAxis ("Vertical");
+
+		rigidBody.AddTorque (transform.up * rotationForce * horizontal);
+	}
+}
