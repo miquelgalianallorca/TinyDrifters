1f1cec44ade6c8515020ec542a4ca937e98f6162
Miquel <miquelgalianallorca@gmail.com>
2017-12-18 19:42:51 +0100

    Car movement fixes
    Starting to test car movement, obstacles, collisions.

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -7,20 +7,19 @@ public class Car : MonoBehaviour {
 	Rigidbody rigidBody;
 	public float accelerationForce = 100;
 	public float rotationForce = 100;
+	public float maxSpeed = 20;
 
 	void Start () {
 		rigidBody = GetComponent<Rigidbody> (); 
 	}
 	
-	// Update is called once per frame
-	void Update () {
-
-		if (Input.GetKeyDown (KeyCode.Space))
-			rigidBody.AddForce (transform.forward * accelerationForce, ForceMode.Acceleration);
-
+	void FixedUpdate () {
+		// Force based movement
 		float horizontal = Input.GetAxis ("Horizontal");
 		float vertical   = Input.GetAxis ("Vertical");
-
 		rigidBody.AddTorque (transform.up * rotationForce * horizontal);
+		rigidBody.AddForce (transform.forward * accelerationForce * vertical, ForceMode.Acceleration);
+
+		rigidBody.velocity = Vector3.ClampMagnitude (rigidBody.velocity, maxSpeed);
 	}
 }
