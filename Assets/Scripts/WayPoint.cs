8063d4d7dd6437fac7f80cf3a05594b8ea76f359
Alejandro Bueno <33668514+goodalexgood@users.noreply.github.com>
2017-12-18 22:53:29 +0100

    First lap detection implementation.

---
+++ Assets/Scripts/WayPoint.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoint : MonoBehaviour {
+
+	public int totalLaps = 3;
+	public int currentLaps = 0;
+	private bool playerEnteredFromBehind;
+	private bool playerEnteredFromFront;
+	public bool wayPointDone = false;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag ("Player")) {
+			float angle = Vector3.Angle (other.attachedRigidbody.velocity, transform.forward);
+
+			if (angle > 90) {
+				playerEnteredFromFront = true;
+				playerEnteredFromBehind = false;
+			} else {
+				playerEnteredFromFront = false;
+				playerEnteredFromBehind = true;
+			}
+
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.CompareTag ("Player")) {
+			float angle = Vector3.Angle (other.attachedRigidbody.velocity, transform.forward);
+			print (angle);
+			if (playerEnteredFromBehind && angle < 90) {
+				currentLaps++;
+			} else if (playerEnteredFromFront && angle > 90) {
+				currentLaps--;
+			}
+
+			wayPointDone = currentLaps >= totalLaps;
+			playerEnteredFromFront = false;
+			playerEnteredFromBehind = false;
+		}
+	}
+}
