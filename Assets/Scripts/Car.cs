e3ce6bbf2bfc15a75877de9a900270a3b397501f
Miquel <miquelgalianallorca@gmail.com>
2017-12-19 22:02:33 +0100

    Car fixes
    Minor fixes.

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -10,10 +10,13 @@ using UnityEngine;
 public class Car : MonoBehaviour {
 
 	public float accelerationForce = 100;
-	public float rotationForce = 100;
+	public float rotationSpeed = 100;
 	public float maxSpeed = 20;
 
 	public Transform carCenter;
+
+	public Transform rightWheel;
+	public Transform leftWheel;
 
 	Rigidbody rigidBody;
 	bool isGrounded = false;
@@ -30,13 +33,38 @@ public class Car : MonoBehaviour {
 		//Debug.Log ("isGrounded: " + isGrounded);
 
 		// Force based movement
-		float horizontal = Input.GetAxis ("Horizontal");
-		float vertical   = Input.GetAxis ("Vertical");
+		float horizontal = 0;
+		float vertical   = 0;
+		if (gameObject.tag == "Player") {
+			horizontal = Input.GetAxis ("Horizontal");
+			vertical   = Input.GetAxis ("Vertical");
+		} else if (gameObject.tag == "Player2") {
+			horizontal = Input.GetAxis ("Horizontal2");
+			vertical   = Input.GetAxis ("Vertical2");
+		}
+
+		float rotValue = rotationSpeed * Time.deltaTime * horizontal;
+		transform.Rotate (Vector3.up, rotValue);
+		RotWheels (rotValue);
 		if (isGrounded) {
-			rigidBody.AddTorque (transform.up * rotationForce * horizontal, ForceMode.Acceleration);
+			//rigidBody.AddTorque (transform.up * rotationForce * horizontal, ForceMode.Acceleration);
 			rigidBody.AddForce (transform.forward * accelerationForce * vertical, ForceMode.Acceleration);
 			rigidBody.velocity = Vector3.ClampMagnitude (rigidBody.velocity, maxSpeed);
 		}
+
 	}
 
+	void RotWheels(float rotValue) {
+		/*
+		if (rightWheel) {
+			float val = 0;
+			if (rotValue > 0) val = Mathf.Clamp (rotValue, 0, 40);
+			else val = Mathf.Clamp (rotValue, -40, 0);
+			rightWheel.Rotate (Vector3.forward, val);
+		}
+		if (leftWheel) {
+			float val = Mathf.Clamp (rotValue, 0, 40);
+			leftWheel.Rotate (Vector3.forward, rotValue);
+		}*/
+	}
 }
