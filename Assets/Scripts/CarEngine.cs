7453277b51112ad6e2f5ed2312da6521187c7f9f
Miquel Galiana Llorca <miquelgalianallorca@gmail.com>
2018-01-04 20:30:01 +0100

    Wheel experiments
    Prototype Car version with Wheel Colliders and minor fixes.

---
+++ Assets/Scripts/CarEngine.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarEngine : MonoBehaviour {
+
+	public WheelCollider wheelFL;
+	public WheelCollider wheelFR;
+	public WheelCollider wheelBL;
+	public WheelCollider wheelBR;
+
+	public float maxSteer = 45f;
+	public float maxTorque = 50f;
+	public float maxSpeed = 100f;
+	float currentSpeed;
+
+	private Rigidbody rigidBody;
+
+	void Start() {
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate () {
+		Steer ();
+		Drive ();
+	}
+
+	void Steer() {
+		// Wheel turn
+		float steer = Input.GetAxis("Horizontal") * maxSteer;
+		wheelFL.steerAngle = steer;
+		wheelFR.steerAngle = steer;
+	}
+
+	void Drive() {
+		currentSpeed = 2f * Mathf.PI * wheelFL.radius * wheelFL.rpm * 60 / 1000;
+		// Wheel advance and back
+		if (currentSpeed < maxSpeed) {
+			float torque = Input.GetAxis ("Vertical") * maxTorque;
+			wheelFL.motorTorque = torque;
+			wheelFR.motorTorque = torque;
+			wheelBL.motorTorque = torque;
+			wheelBR.motorTorque = torque;
+		} else {
+			wheelFL.motorTorque = 0;
+			wheelFR.motorTorque = 0;
+			wheelBL.motorTorque = 0;
+			wheelBR.motorTorque = 0;
+		}
+	}
+}
---
+++ Assets/Scripts/CarWheel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarWheel : MonoBehaviour {
+
+	public WheelCollider collider;
+
+	private Vector3 position = new Vector3();
+	private Quaternion rotation = new Quaternion ();
+
+	void Update () {
+		collider.GetWorldPose (out position, out rotation);
+		transform.position = position;
+		//transform.rotation = rotation; //IN THEORY this is enough
+		Vector3 rotEu = rotation.eulerAngles;
+		//transform.rotation = Quaternion.Euler(new Vector3(-0, 0, 0));
+		//Debug.Log (rotEu);
+		transform.Rotate(0, rotEu.y, rotEu.z);
+	}
+}
